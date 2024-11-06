0c87d1534358ebba6794ea4a7b1ed84d027ff4e3
0x00646f616e6e63 <doannc2212@gmail.com>
2024-11-06 16:56:02 +0700

    refactor: seperated codebase to monorepo

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { LanguageProvider } from "@inlang/paraglide-next";
-import { languageTag } from "@/paraglide/runtime.js";
+import { languageTag } from "@/paraglide/runtime";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
