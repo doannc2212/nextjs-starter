3e8b91afd57a707ae13da02dcacfd4a5482438c4
Nguyen Cong Doan <doannc2212@gmail.com>
2024-11-12 14:46:30 +0700

    chore: added sample test

---
+++ apps/web/cypress/e2e/spec.cy.ts
@@ -0,0 +1,5 @@
+describe("As a user, I can see the full home page", () => {
+  it("should visible on screen", () => {
+    cy.visit("/");
+  });
+});
--- apps/web/src/app/pageHome.cy.tsx
+++ apps/web/src/app/pageHome.cy.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import Home from './page'
+import React from "react";
+import Home from "./page";
 
-describe('<Home />', () => {
-  it('renders', () => {
+describe("<Home />", () => {
+  it("renders", () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Home />)
-  })
-})
+    cy.mount(<Home />);
+
+    const url =
+      "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app";
+    cy.get(`a[href=${url}]`).should("be.visible");
+  });
+});
