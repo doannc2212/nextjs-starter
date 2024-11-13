d0bf2a377ef2f8473cecbe0a7b3ce8ab6fa4d934
0x00646f616e6e63 <doannc2212@gmail.com>
2024-11-13 11:09:12 +0700

    chore: run code format

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -3,7 +3,11 @@ module.exports = {
   root: true,
   extends: ["@nextjs-starter/eslint-config"],
   parser: "@typescript-eslint/parser",
+
   parserOptions: {
     project: true,
+  },
+  rules: {
+    "@next/next/no-html-link-for-pages": 0,
   },
 };
