ba992388464589122ebecbc6dc59c41c576043a3
0x00646f616e6e63 <doannc2212@gmail.com>
2025-02-11 11:10:30 +0700

    fix: build failure by misconfiguration secure header

--- packages/security/middleware.ts
+++ packages/security/middleware.ts
@@ -7,9 +7,8 @@ export function secureHeader(
 ) {
   // https://github.com/arcjet/arcjet-js/blob/02e4435a86b6b40b97feb369f0402b2199a4bc12/nosecone-next/index.ts#L41-L59
   const headers = nosecone(options);
-  response.headers.entries().forEach(([key, value]) => {
+  for (const [key, value] of response.headers.entries()) {
     if (!headers.has(key)) headers.set(key, value);
-  });
-  headers.set("x-middleware-next", "1");
+  }
   return new NextResponse(response.body, { headers });
 }
